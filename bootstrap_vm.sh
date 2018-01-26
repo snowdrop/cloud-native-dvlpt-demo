fdd485c1308458b0f6b69f8d02d739c9b8996bc9
Charles Moulliard <cmoulliard@redhat.com>
2018-01-26 11:31:16 +0100

    Add boolean to enable/disable image_cache

--- bootstrap_vm.sh
+++ bootstrap_vm.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 
+#
+# Command Usage
+# ./bootstrap_vm.sh [COMMANDS]
+#
+# where commands are:
+# istio version       Version of istio to be used. Default to 0.4.0
+# ocp version         Version of Openshift Origin. Default to : 3.7.1
+# imageCache          Enable or disable to use docker imges cached from the disk. Default is false
+#
+# ./bootstrap_vm.sh 0.4.0 3.7.1 true
+#
+
 ISTIO_PROFILE_DIR="$HOME/.minishift/profiles/istio"
 ISTIO_VERSION=${1:-0.4.0}
 OCP_VERSION=${2:-3.7.1}
+IMAGE_CACHE=${3:-false}
 
 docker_images=(
-  #  istio/istio-ca:$ISTIO_VERSION
-  #  istio/grafana:$ISTIO_VERSION
-  #  istio/pilot:$ISTIO_VERSION
-  #  istio/proxy_debug:$ISTIO_VERSION
-  #  istio/proxy_init:$ISTIO_VERSION
-  #  istio/mixer:$ISTIO_VERSION
-  #  istio/servicegraph:$ISTIO_VERSION
-  #  prom/statsd-exporter:v0.5.0
-  #  prom/prometheus:v2.0.0
-  #  alpine:latest
-  #  jaegertracing/all-in-one:latest
-  #  openshift/origin-docker-registry:v$OCP_VERSION
-  #  openshift/origin-haproxy-router:v$OCP_VERSION
-  #  openshift/origin-deployer:v$OCP_VERSION
-  #  openshift/origin:v$OCP_VERSION
-  #  openshift/origin-pod:v$OCP_VERSION
-  #  openshift/origin-sti-builder:v$OCP_VERSION
+  istio/istio-ca:$ISTIO_VERSION
+  istio/grafana:$ISTIO_VERSION
+  istio/pilot:$ISTIO_VERSION
+  istio/proxy_debug:$ISTIO_VERSION
+  istio/proxy_init:$ISTIO_VERSION
+  istio/mixer:$ISTIO_VERSION
+  istio/servicegraph:$ISTIO_VERSION
+  prom/statsd-exporter:v0.5.0
+  prom/prometheus:v2.0.0
+  alpine:latest
+  jaegertracing/all-in-one:latest
+  openshift/origin-docker-registry:v$OCP_VERSION
+  openshift/origin-haproxy-router:v$OCP_VERSION
+  openshift/origin-deployer:v$OCP_VERSION
+  openshift/origin:v$OCP_VERSION
+  openshift/origin-pod:v$OCP_VERSION
+  openshift/origin-sti-builder:v$OCP_VERSION
   fabric8/s2i-java:2.0
   fabric8/configmapcontroller:2.3.7
 )
@@ -36,13 +49,18 @@ if [ ! -d "$ISTIO_PROFILE_DIR" ]; then
 fi
 
 minishift config set image-caching true
-minishift image cache-config add $IMAGES
+
+if [ "$IMAGE_CACHE" = true ] ; then
+  minishift image cache-config add $IMAGES
+fi
 
 #minishift start --profile istio
 MINISHIFT_ENABLE_EXPERIMENTAL=y minishift start --profile istio --service-catalog
 
-# Export images to be sure to have a backup locally
-minishift image export
+if [ "$IMAGE_CACHE" = true ] ; then
+  # Export images to be sure to have a backup locally
+  minishift image export
+fi
 
 echo "Log to OpenShift using admin user"
 oc login -u system:admin
